fd1691cfefba659ad274eb64ae5eea6b3c032c6e
myselfcarvajal <myselfcarvajal@gmail.com>
2025-02-19 11:44:42 -0500

    fix: make pagination properties optional in Swagger

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,19 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsOptional, IsPositive, IsString, Min } from 'class-validator';
 
 export class PaginationDto {
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsPositive()
   @Type(() => Number) // enableImplicitConversions: true
   limit?: number;
-  
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @Min(0)
   @Type(() => Number) // enableImplicitConversions: true
   offset?: number;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
-  name?: string
+  name?: string;
 }
