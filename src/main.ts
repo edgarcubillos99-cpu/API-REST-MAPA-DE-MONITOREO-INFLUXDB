f09cecb5eef838fc5005bc25d723bc586dda2c15
myselfcarvajal <myselfcarvajal@gmail.com>
2025-02-27 10:38:20 -0500

    refactor: removing cluster

--- src/main.ts
+++ src/main.ts
@@ -2,49 +2,31 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-const cluster = require('cluster');
-
-export const myclusterPrimary = 0;
 
 async function bootstrap() {
-  if (cluster.isPrimary) {
-    for (let i = 0; i < 8; i++) {
-      cluster.fork();
-    }
-    // Si un worker muere, lo reiniciamos
-    cluster.on('exit', (worker) => {
-      console.log(`Worker ${worker.process.pid} died. Restarting...`);
-      cluster.fork();
-    });
-  } else {
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-    //CONFIGURANDO PIPES GLOBALES
-    app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-      }),
-    );
+  //CONFIGURANDO PIPES GLOBALES
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
-    //SI EL ENTORNO ES DE DESARROLLO
-    if (process.env.ENTORNO === 'DEV') {
-      const config = new DocumentBuilder()
-        .setTitle('MDM Documentation')
-        .setDescription('Mapa de monitoreo api docs')
-        .setVersion('1.0')
-        .addBearerAuth()
-        .build();
+  //SI EL ENTORNO ES DE DESARROLLO
+  if (process.env.ENTORNO === 'DEV') {
+    const config = new DocumentBuilder()
+      .setTitle('MDM Documentation')
+      .setDescription('Mapa de monitoreo api docs')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
 
-      const document = SwaggerModule.createDocument(app, config);
-      SwaggerModule.setup('docs', app, document);
-    }
-
-    await app.listen(process.env.PORT ?? 3000);
-
-    console.log(
-      `Worker server started on ${process.pid} cluster id ${cluster.worker.id}`,
-    );
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
   }
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
